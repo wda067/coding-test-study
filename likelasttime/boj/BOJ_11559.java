90b07583d71aa8afbbe9fa17a732fab4cc330c0c
강민정 <a50070863@gmail.com>
2026-01-23 16:29:58 +0900

    refactor: BOJ_11559

--- likelasttime/boj/BOJ_11559.java
+++ likelasttime/boj/BOJ_11559.java
@@ -7,11 +7,11 @@ import java.util.*;
     터질 수 있는 뿌요가 여러 그룹이 있다면 동시에 터져야 하고 여러 그룹이 터지더라도 한번의 연쇄가 추가된다.
     연쇄가 몇 번 연속으로 일어날지 계산하기
 */
-class BOJ_11559 {
+class Main {
     final static int[] DX = {-1, 1, 0, 0};
     final static int[] DY = {0, 0, -1, 1};
-    final static int ROW_SIZE = 12;
-    final static int COL_SIZE = 6;
+    final static int ROW_SIZE = 12;		// 세로 크기
+    final static int COL_SIZE = 6;		// 가로 크기
     final static char R = 'R';
     final static char G = 'G';
     final static char B = 'B';
@@ -19,9 +19,8 @@ class BOJ_11559 {
     final static char Y = 'Y';
     final static char EMPTY = '.';
 
-    static char[][] arr;
-    static char[][] copied;
-    static int answer = 0;
+    static char[][] arr;		// ROW_SIZE * COL_SIZE 크기의 배열
+    static int answer = 0;		// 연쇄 횟수
 
     static class Position {
         int x;
@@ -33,24 +32,20 @@ class BOJ_11559 {
         }
     }
 
+    /*
+     * (x, y)가 격자 내에 있다면 true 반환
+     */
     public static boolean inRange(int x, int y) {
         return 0 <= x && x < ROW_SIZE && 0 <= y && y < COL_SIZE;
     }
 
     /*
-     * origin을 target에 복사
+     * (x, y) 뿌요에서 시작해서 상하좌우로 인접한 같은 뿌요를 찾는 BFS 탐색
+     * 4개 이상 찾으면 해당 뿌요들은 사라지고, true 반환
      */
-    public static void copy(char[][] origin, char[][] target) {
-        for(int x=0; x<ROW_SIZE; x++) {
-            for(int y=0; y<COL_SIZE; y++) {
-                target[x][y] = origin[x][y];
-            }
-        }
-    }
-
     public static boolean bfs(int x, int y, boolean[][] visit) {
         Queue<Position> que = new LinkedList();
-        List<Position> lst = new ArrayList();
+        List<Position> lst = new ArrayList();		// arr[x][y]와 같은 뿌요의 위치를 담는 리스트
         int cnt = 0;		// 같은 색깔의 뿌요 갯수
         lst.add(new Position(x, y));
         que.offer(new Position(x, y));
@@ -71,14 +66,14 @@ class BOJ_11559 {
             }
         }
 
-        if(cnt < 4) {
+        if(cnt < 4) {	// 같은 색 뿌요가 4개 미만이라면
             return false;
         }
 
         // 같은 색 뿌요가 4개 이상 상하좌우로 연결되어 있으면
         for(Position cur : lst) {
             // 연결된 같은 색 뿌요들이 한꺼번에 없어진다.
-            copied[cur.x][cur.y] = EMPTY;
+            arr[cur.x][cur.y] = EMPTY;
         }
 
         return true;
@@ -94,24 +89,19 @@ class BOJ_11559 {
 
             // 아래 → 위로 뿌요 수집
             for (int x = ROW_SIZE - 1; x >= 0; x--) {
-                if (copied[x][y] != EMPTY) {		// 빈 공간이 아니면(뿌요)
-                    arr[writeX][y] = copied[x][y];
-                    writeX--;
+                if (arr[x][y] != EMPTY) {		// 빈 공간이 아니면(뿌요)
+                    char tmp = arr[x][y];
+                    arr[x][y] = EMPTY;
+                    arr[writeX--][y] = tmp;
                 }
-            }
-
-            // 남은 칸은 빈칸으로
-            for (int x = writeX; x >= 0; x--) {
-                arr[x][y] = '.';
             }
         }
     }
 
     public static void simulate() {
-        while(true) {
-            boolean flag = false;
+        while(true) {	// 뿌요가 연쇄적으로 터질동안
+            boolean flag = false;		// 배열 전체를 탐색하며 연쇄적으로 터진 뿌요가 있는지
             boolean[][] visit = new boolean[ROW_SIZE][COL_SIZE];
-            copy(arr, copied);		// arr을 copied로 복사
             for(int x=0; x<ROW_SIZE; x++) {
                 for(int y=0; y<COL_SIZE; y++) {
                     // 빈 공간(.)이거나 이미 방문했거나
@@ -119,7 +109,7 @@ class BOJ_11559 {
                         continue;
                     }
                     boolean result = bfs(x, y, visit);
-                    if(result) {
+                    if(result) {	// 연쇄적으로 터진 뿌요가 있다면
                         flag = true;
                     }
                 }
@@ -138,7 +128,6 @@ class BOJ_11559 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         arr = new char[ROW_SIZE][COL_SIZE];
-        copied = new char[ROW_SIZE][COL_SIZE];
         for(int x=0; x<ROW_SIZE; x++) {
             String str = sc.next();
             for(int y=0; y<COL_SIZE; y++) {
