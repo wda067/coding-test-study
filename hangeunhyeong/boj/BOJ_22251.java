e4acb462ba723a3f3993e55d70124deed3aa0c25
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-03-05 00:50:31 +0900

    feat: BOJ_22251 수정

--- hangeunhyeong/boj/BOJ_22251.java
+++ hangeunhyeong/boj/BOJ_22251.java
@@ -20,28 +20,22 @@ public class BOJ_22251 {
         K = Integer.parseInt(st.nextToken());
         P = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-        int[] x = display(X);
-        int[] bit = {0b1110111, 0b0100100, 0b1011101, 0b1101101, 0b0101110, 0b1101011, 0b1111011, 0b0100101, 0b1111111, 0b1101111, 0b1110111};
+        int[] bit = {0b1110111, 0b0100100, 0b1011101, 0b1101101, 0b0101110, 0b1101011, 0b1111011, 0b0100101, 0b1111111, 0b1101111};
         int answer = 0;
         for(int i = 1; i <= N; i++){
-            int[] cmp = display(i);
+            int a = X;
+            int b = i;
             int cnt = 0;
             for(int j = 0; j < K; j++){
-                int a = bit[x[j]];   int b = bit[cmp[j]];
-                cnt += count(a ^ b);
+                int n = bit[a % 10];   int m = bit[b % 10];
+                cnt += Integer.bitCount(n ^ m);
+                a /= 10;
+                b /= 10;
             }
             if(cnt <= P)
                 answer++;
         }
         System.out.println(answer - 1);
-    }
-    public static int[] display(int X){
-        int[] x = new int[K];
-        for(int i = 0; X != 0 && i < K; i++){
-            x[i] = X % 10;
-            X = X / 10;
-        }
-        return x;
     }
     // 2진수 x에서 1의 개수 세기
     public static int count(int x){
